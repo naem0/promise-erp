709e7158ba13114c43228bef3c47602032997b5b
MD Naem <121675549+naem0@users.noreply.github.com>
2026-04-05 16:47:49 +0600

    feat: implement admin dashboard layout with sidebar navigation and suspense fallback

--- app/(admin-dashboard)/layout.tsx
+++ app/(admin-dashboard)/layout.tsx
@@ -6,22 +6,22 @@ import { Separator } from "@/components/ui/separator"
 export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
 
   return (
-          <SidebarProvider>
-        <Suspense fallback={<div className="w-64 h-screen bg-muted animate-pulse" />}>
-          <AppSidebar />
-        </Suspense>
-        <main className="w-full">
-          <SidebarInset>
-            <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-              <SidebarTrigger className="-ml-1" />
-              <Separator
-                orientation="vertical"
-                className="mr-2 data-[orientation=vertical]:h-4"
-              />
-            </header>
-            <div className="">{children}</div>
-          </SidebarInset>
-        </main>
-      </SidebarProvider>
-      )
+    <SidebarProvider>
+      <Suspense fallback={<div className="w-64 h-screen bg-muted animate-pulse" />}>
+        <AppSidebar />
+      </Suspense>
+      <main className="w-full">
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mr-2 data-[orientation=vertical]:h-4"
+            />
+          </header>
+          <div className="">{children}</div>
+        </SidebarInset>
+      </main>
+    </SidebarProvider>
+  )
 }
